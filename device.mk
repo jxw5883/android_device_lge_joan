f2858452033384d701889555545f5d2b44563368
SupremMotu <francisd132435@gmail.com>
2021-03-16 09:38:01 -0700

    joan: Stay Paranoid

--- device.mk
+++ device.mk
@@ -418,9 +418,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Tethering
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
